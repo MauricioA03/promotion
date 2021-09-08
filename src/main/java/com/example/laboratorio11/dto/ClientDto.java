17f33ba5a8874a0899f633b640260df4c13c40f1
MauricioArancibia <mauricio.crispink@gmail.com>
2021-09-08 15:23:11 -0400

    add Entity Client, ClientDto, ModelBase

--- src/main/java/com/example/laboratorio11/dto/ClientDto.java
+++ src/main/java/com/example/laboratorio11/dto/ClientDto.java
@@ -2,7 +2,6 @@ package com.example.laboratorio11.dto;
 
 
 import com.example.laboratorio11.model.Client;
-import org.modelmapper.ModelMapper;
 
 public class ClientDto extends DtoBase<Client> {
 
--- src/main/java/com/example/laboratorio11/model/Client.java
+++ src/main/java/com/example/laboratorio11/model/Client.java
@@ -1,14 +1,12 @@
 package com.example.laboratorio11.model;
 
 
-
 import com.example.laboratorio11.dto.ClientDto;
 
 import javax.persistence.Entity;
 
 @Entity
 public class Client extends ModelBase<ClientDto> {
-
 
 
 }
